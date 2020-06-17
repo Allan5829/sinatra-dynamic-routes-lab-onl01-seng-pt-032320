8bb515b222441a1e88c97aa2d8d744bef4899ebd
Allan Farca <allan.farca@gmail.com>
2020-06-17 06:10:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,8 +15,9 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
     @number = params[:number]
     @phrase = params[:phrase]
+    string = ""
     @number.to_i.times do
-      "#{@phrase}"
+      total = [string, @phrase].join
     end
   end
 
