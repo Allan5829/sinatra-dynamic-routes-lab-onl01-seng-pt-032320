b1f8c197ed7574d675d80340bb864bbf749effd0
Allan Farca <allan.farca@gmail.com>
2020-06-17 06:05:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,10 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get "/reversename/:name" do
     @name = params[:name]
-    array = "#{@name}".split()
-    rarray = array.reverse()
-    puts rarray
-    "#{rarray.join}"
+    "#{@name.reverse}"
   end
 
   get "/square/:number" do
